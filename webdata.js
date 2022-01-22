b3449b61d68a51bcf3fd9f7ce326d80189f1f93b
Olli Hollmen <olli.hollmen@gmail.com>
2022-01-21 19:45:28 -0800

    chart_timelim_idx: Default to useing end-of-date as reference, but add refnow flag. Only produce verbose log/debug output when debug=1

--- webdata.js
+++ webdata.js
@@ -146,7 +146,6 @@ webdata.chart_colorset = function (cdata, colarr) {
   cdata.datasets.forEach((ds) => {
     ds.borderColor = colarr[i];
     ds.borderWidth = 1;
-    
     i++;
     // if (i >= colarr.length) { i = 0; }
   });
@@ -155,28 +154,33 @@ webdata.chart_colorset = function (cdata, colarr) {
 * Assumes all labels (cdata.labels) to be in (JS Data()) parseable time format.
 * Tested with ISO time, not sure if others work.
 * Note: Only labels are accessed during this op.
+* @param cdata - Chart data
+* @param ts - Time in second
+* @return The index of first element to keep
 */
 webdata.chart_timelim_idx = function (cdata, ts, opts) {
-  opts = opts || {copy: 0, debug: 0};
+  opts = opts || {copy: 0, debug: 0, refnow: 0};
   if (!ts) { console.log("Got:"+ts); return; }
-  var ut = Math.floor(Date.now() / 1000); // new Date();
+  // TODO: Covert all to use ms (ts, ut, dt) ts*=1000;
+  var ut;
+  if (opts.refnow) { ut = Math.floor(Date.now() / 1000); } // new Date();
+  // Better reference - end of data. Check dl
+  else { let dl = webdata.chart_lastdate(cdata); ut = Math.floor(dl / 1000); }
   var dt = ut - ts;
   dt = dt*1000; // 
   var d = new Date(dt);
   if (opts.debug) {
-  console.log("ST("+dt+"): ", d);
-  console.log("Time at "+dt+" ms. is "+d.toISOString());
-  console.log("labels cnt:", cdata.labels.length);
+    console.log("ST("+dt+"): ", d);
+    console.log("Time at "+dt+" ms. is "+d.toISOString());
+    console.log("labels cnt:", cdata.labels.length);
   }
   var i = 0;
   var dls = cdata.labels;
   for (;i<dls.length;i++) {
     let d2 = new Date(dls[i]); // Date.parse();
     //console.log(d2.toISOString());
-    //var t1 = d; //.getMilliseconds();
-    //var t2 = d2; // .getMilliseconds();
-    console.log("CMP("+i+"): "+d+" "+d2);
-    // if (d2.getMilliseconds() > d.getMilliseconds()) { break; }
+    // Note: .getMilliseconds(); gets ONLY the ms part
+    opts.debug && console.log("CMP("+i+"): "+d+" "+d2);
     if (d2 > d) { break; }
   }
   if (i >= dls.length) { console.log("Reached end ("+i+") No match !!!"); return -1; }
